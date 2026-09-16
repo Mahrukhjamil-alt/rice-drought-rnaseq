acf9d5129dc32d1ac736336427275941dec2bd12
Mahrukhjamil-alt <mahrukhjamil0@gmail.com>
2026-09-16 09:46:22 +0500

    Update 01_fastqc.sh

--- bash/01_fastqc.sh
+++ bash/01_fastqc.sh
@@ -1,1 +1,15 @@
-
+fastqc \
+/mnt/d/rnaseq_data/SRR36685773_1.fastq.gz \
+/mnt/d/rnaseq_data/SRR36685773_2.fastq.gz \
+/mnt/d/rnaseq_data/SRR36685782_1.fastq.gz \
+/mnt/d/rnaseq_data/SRR36685782_2.fastq.gz \
+/mnt/d/rnaseq_data/SRR36685783_1.fastq.gz \
+/mnt/d/rnaseq_data/SRR36685783_2.fastq.gz \
+/mnt/d/rnaseq_data/SRR36685784_1.fastq.gz \
+/mnt/d/rnaseq_data/SRR36685784_2.fastq.gz \
+/mnt/d/rnaseq_data/SRR36685785_1.fastq.gz \
+/mnt/d/rnaseq_data/SRR36685785_2.fastq.gz \
+/mnt/d/rnaseq_data/SRR36685786_1.fastq.gz \
+/mnt/d/rnaseq_data/SRR36685786_2.fastq.gz \
+-o /mnt/d/rnaseq_data/fastqc_results \
+-t 4
