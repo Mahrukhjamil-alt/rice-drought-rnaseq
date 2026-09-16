933ce43613c9fc968678499effa3d49070547ae5
Mahrukhjamil-alt <mahrukhjamil0@gmail.com>
2026-09-16 09:47:13 +0500

    Update 05_featureCounts.sh

--- bash/05_featureCounts.sh
+++ bash/05_featureCounts.sh
@@ -1,1 +1,5 @@
-
+featureCounts \
+-p -T 4 -s 2 -t exon -g gene_id \
+-a /mnt/d/rnaseq_data/reference/Oryza_sativa.IRGSP-1.0.63.gtf \
+-o /mnt/d/rnaseq_data/counts/gene_counts.txt \
+/mnt/d/rnaseq_data/aligned/*_sorted.bam
