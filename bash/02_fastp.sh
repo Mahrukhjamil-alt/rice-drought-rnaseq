fac7d312c86e3d164d7a7d48d08f5cd3101e4b20
Mahrukhjamil-alt <mahrukhjamil0@gmail.com>
2026-09-16 09:49:08 +0500

    Update 02_fastp.sh

--- bash/02_fastp.sh
+++ bash/02_fastp.sh
@@ -1,1 +1,41 @@
+fastp -i SRR36685773_1.fastq -I SRR36685773_2.fastq \
+      -o /mnt/d/rnaseq_data/trimmed/SRR36685773_1_trimmed.fastq \
+      -O /mnt/d/rnaseq_data/trimmed/SRR36685773_2_trimmed.fastq \
+      -h /mnt/d/rnaseq_data/trimmed/SRR36685773_report.html \
+      -j /mnt/d/rnaseq_data/trimmed/SRR36685773_report.json \
+      -w 4
 
+fastp -i SRR36685782_1.fastq -I SRR36685782_2.fastq \
+      -o /mnt/d/rnaseq_data/trimmed/SRR36685782_1_trimmed.fastq \
+      -O /mnt/d/rnaseq_data/trimmed/SRR36685782_2_trimmed.fastq \
+      -h /mnt/d/rnaseq_data/trimmed/SRR36685782_report.html \
+      -j /mnt/d/rnaseq_data/trimmed/SRR36685782_report.json \
+      -w 4
+
+fastp -i SRR36685783_1.fastq -I SRR36685783_2.fastq \
+      -o /mnt/d/rnaseq_data/trimmed/SRR36685783_1_trimmed.fastq \
+      -O /mnt/d/rnaseq_data/trimmed/SRR36685783_2_trimmed.fastq \
+      -h /mnt/d/rnaseq_data/trimmed/SRR36685783_report.html \
+      -j /mnt/d/rnaseq_data/trimmed/SRR36685783_report.json \
+      -w 4
+
+fastp -i SRR36685784_1.fastq -I SRR36685784_2.fastq \
+      -o /mnt/d/rnaseq_data/trimmed/SRR36685784_1_trimmed.fastq \
+      -O /mnt/d/rnaseq_data/trimmed/SRR36685784_2_trimmed.fastq \
+      -h /mnt/d/rnaseq_data/trimmed/SRR36685784_report.html \
+      -j /mnt/d/rnaseq_data/trimmed/SRR36685784_report.json \
+      -w 4
+
+fastp -i SRR36685785_1.fastq -I SRR36685785_2.fastq \
+      -o /mnt/d/rnaseq_data/trimmed/SRR36685785_1_trimmed.fastq \
+      -O /mnt/d/rnaseq_data/trimmed/SRR36685785_2_trimmed.fastq \
+      -h /mnt/d/rnaseq_data/trimmed/SRR36685785_report.html \
+      -j /mnt/d/rnaseq_data/trimmed/SRR36685785_report.json \
+      -w 4
+
+fastp -i SRR36685786_1.fastq -I SRR36685786_2.fastq \
+      -o /mnt/d/rnaseq_data/trimmed/SRR36685786_1_trimmed.fastq \
+      -O /mnt/d/rnaseq_data/trimmed/SRR36685786_2_trimmed.fastq \
+      -h /mnt/d/rnaseq_data/trimmed/SRR36685786_report.html \
+      -j /mnt/d/rnaseq_data/trimmed/SRR36685786_report.json \
+      -w 4
