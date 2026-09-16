1cec82802e1f3468574a252dfcf44f618a16b310
Mahrukhjamil-alt <mahrukhjamil0@gmail.com>
2026-09-16 09:51:18 +0500

    Update 04_hisat2.sh

--- bash/04_hisat2.sh
+++ bash/04_hisat2.sh
@@ -1,1 +1,41 @@
+hisat2 -p 4 \
+-x /mnt/d/rnaseq_data/reference/rice_index \
+-1 /mnt/d/rnaseq_data/trimmed/SRR36685773_1_trimmed.fastq \
+-2 /mnt/d/rnaseq_data/trimmed/SRR36685773_2_trimmed.fastq \
+-S /mnt/d/rnaseq_data/aligned/SRR36685773.sam \
+2> /mnt/d/rnaseq_data/aligned/SRR36685773hisat2.log
 
+hisat2 -p 4 \
+-x /mnt/d/rnaseq_data/reference/rice_index \
+-1 /mnt/d/rnaseq_data/trimmed/SRR36685782_1_trimmed.fastq \
+-2 /mnt/d/rnaseq_data/trimmed/SRR36685782_2_trimmed.fastq \
+-S /mnt/d/rnaseq_data/aligned/SRR36685782.sam \
+2> /mnt/d/rnaseq_data/aligned/SRR36685782hisat2.log
+
+hisat2 -p 4 \
+-x /mnt/d/rnaseq_data/reference/rice_index \
+-1 /mnt/d/rnaseq_data/trimmed/SRR36685783_1_trimmed.fastq \
+-2 /mnt/d/rnaseq_data/trimmed/SRR36685783_2_trimmed.fastq \
+-S /mnt/d/rnaseq_data/aligned/SRR36685783.sam \
+2> /mnt/d/rnaseq_data/aligned/SRR36685783hisat2.log
+
+hisat2 -p 4 \
+-x /mnt/d/rnaseq_data/reference/rice_index \
+-1 /mnt/d/rnaseq_data/trimmed/SRR36685784_1_trimmed.fastq \
+-2 /mnt/d/rnaseq_data/trimmed/SRR36685784_2_trimmed.fastq \
+-S /mnt/d/rnaseq_data/aligned/SRR36685784.sam \
+2> /mnt/d/rnaseq_data/aligned/SRR36685784hisat2.log
+
+hisat2 -p 4 \
+-x /mnt/d/rnaseq_data/reference/rice_index \
+-1 /mnt/d/rnaseq_data/trimmed/SRR36685785_1_trimmed.fastq \
+-2 /mnt/d/rnaseq_data/trimmed/SRR36685785_2_trimmed.fastq \
+-S /mnt/d/rnaseq_data/aligned/SRR36685785.sam \
+2> /mnt/d/rnaseq_data/aligned/SRR36685785hisat2.log
+
+hisat2 -p 4 \
+-x /mnt/d/rnaseq_data/reference/rice_index \
+-1 /mnt/d/rnaseq_data/trimmed/SRR36685786_1_trimmed.fastq \
+-2 /mnt/d/rnaseq_data/trimmed/SRR36685786_2_trimmed.fastq \
+-S /mnt/d/rnaseq_data/aligned/SRR36685786.sam \
+2> /mnt/d/rnaseq_data/aligned/SRR36685786hisat2.log
